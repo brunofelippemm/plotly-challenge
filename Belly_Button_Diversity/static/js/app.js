3daf7a79e672499270618225fc6270375e8764e7
Bruno Miranda <brunofelippemm@gmail.com>
2019-09-21 11:59:12 -0500

    add initdb

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -3,27 +3,76 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(`/metadata/${sample}`).then(function(sampleData){
+
     // Use d3 to select the panel with id of `#sample-metadata`
+    let samplePanel = d3.select('#sample-metadata');
 
     // Use `.html("") to clear any existing metadata
+    samplePanel.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(sampleData).forEach(([key, value]) => {
+      samplePanel.append('h5').text(`${key}: ${value}`);
+    })
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+  })
+}
+
+const first10 = (firstObs) => {
+  return firstObs.slice(0,10);
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(`/samples/${sample}`).then(function(sampleData){
+    let otuIds = sampleData.otu_ids;
+    let otuLabels = sampleData.otu_labels;
+    let sampleValues = sampleData.sample_values
 
     // @TODO: Build a Bubble Chart using the sample data
+    let bubbleChart = [{
+      x: otuIds,
+      y: sampleValues,
+      text: otuLabels,
+      mode: 'markers',
+      marker:{
+        size: sampleValues,
+        color: otuIds,
+        colorscale: 'Earth'
+      }
+    }];
 
+    let bubbleStyle = {
+      margint: {t:0},
+      hovermode: 'closest',
+      xaxis:{title: 'Otu ID'},
+    };
+
+    Plotly.plot('bubble', bubbleChart, bubbleStyle);
+    
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    let pieChart = [{
+      values: first10(sampleValues),
+      labels: first10(otuIds),
+      hovertext: first10(otuLabels),
+      hoverinfo: 'hovertext',
+      type: 'pie'
+    }];
+
+    let pieStyle = {
+      margin: {t: 0, l:0}
+    }
+
+    Plotly.plot('pie', pieChart, pieStyle)
+    });
 }
 
 function init() {
